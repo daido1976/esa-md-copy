2d080009871dda893cf2597464e08790af76253e
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-05-16 21:57:50 +0900

    Enable to work

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-let prevContainer = null;
+let prevSelectedNode = null;
 
-const parseListItem = (list) => {
-  const listItem = list.querySelector("li:nth-child(1)");
-  const imgTitle = listItem.querySelector("img").title;
-  const content = listItem.innerText.split("\n")[0];
-  return `- ${imgTitle}${content}`;
+const parseList = (listNode) => {
+  let ary = [];
+  for (const child of listNode.children) {
+    let str = "";
+    for (let node of child.childNodes) {
+      switch (node.nodeName) {
+        case "IMG":
+          str += node.title;
+          break;
+        case "#text":
+          if (node.nodeValue === "\n") {
+            break;
+          }
+          str += node.nodeValue;
+          break;
+        default:
+          break;
+      }
+    }
+    ary.push(`- ${str}`);
+  }
+  return ary.join("\n");
 };
 
 const logSelection = () => {
   window.setTimeout(() => {
-    const container = document.getSelection()?.getRangeAt(0)
+    const selectedNode = document.getSelection()?.getRangeAt(0)
       .commonAncestorContainer;
-    if (prevContainer === container) {
+    if (prevSelectedNode === selectedNode) {
       return;
     }
-    prevContainer = container;
-    console.log(container);
-    console.log(parseListItem(container));
+    prevSelectedNode = selectedNode;
+    console.log(selectedNode);
+    console.log(parseList(selectedNode));
   }, 2000);
 };
 
