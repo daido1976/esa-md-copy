14754725c5744a349c9aa9b57df39bc818fa134b
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-05-16 16:37:53 +0900

    Print selection container to console

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello");
+const logSelection = () => {
+  const container = document.getSelection().getRangeAt(0)
+    .commonAncestorContainer;
+  console.log(container);
+};
+
+document.addEventListener("selectionchange", logSelection);
